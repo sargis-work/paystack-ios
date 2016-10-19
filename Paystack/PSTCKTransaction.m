7617761a643fd706adb1ef2504e4021e4339321f
ibrahimlawal <ibrahim@lawal.me>
2016-10-19 11:08:54 +0100

    Add Transaction Class

---
+++ Paystack/PSTCKTransaction.m
@@ -0,0 +1,79 @@
+//
+//  PSTCKTransaction.m
+//  Paystack
+//
+
+#import "PSTCKTransaction.h"
+#import "PSTCKCard.h"
+#import "NSDictionary+Paystack.h"
+
+@interface PSTCKTransaction()
+@property (nonatomic) NSString *reference;
+@property (nonatomic) NSString *message;
+@property (nonatomic) NSString *trans;
+@property (nonatomic) NSString *redirecturl;
+@property (nonatomic, readwrite, nonnull, copy) NSDictionary *allResponseFields;
+@end
+
+@implementation PSTCKTransaction
+
+- (NSString *)description {
+    return self.reference ?: self.message ?: @"Unknown reference";
+}
+
+- (NSString *)debugDescription {
+    NSString *reference = self.reference ?: @"Unknown Reference";
+    NSString *message = self.message ?: @"Unknown Message";
+    return [NSString stringWithFormat:@"%@ (%@)", reference, message];
+}
+
+- (BOOL)isEqual:(id)object {
+    return [self isEqualToTransaction:object];
+}
+
+- (NSUInteger)hash {
+    return [self.reference hash];
+}
+
+- (BOOL)isEqualToTransaction:(PSTCKTransaction *)object {
+    if (self == object) {
+        return YES;
+    }
+
+    if (!object || ![object isKindOfClass:self.class]) {
+        return NO;
+    }
+
+    return [self.reference isEqualToString:object.reference] &&[self.message isEqualToString:object.message] &&
+              [self.trans isEqualToString:object.trans] ;
+    
+}
+
+#pragma mark PSTCKAPIResponseDecodable
+
++ (NSArray *)requiredFields {
+    //return @[@"id", @"livemode", @"created"];
+    return @[@"status", @"message"];
+}
+
++ (instancetype)decodedObjectFromAPIResponse:(NSDictionary *)response {
+    NSDictionary *dict = [response pstck_dictionaryByRemovingNullsValidatingRequiredFields:[self requiredFields]];
+    if (!dict) {
+        return nil;
+    }
+    
+    if (![[dict[@"status"] description] isEqual: @"1"]) {
+        return nil;
+    }
+    
+    PSTCKTransaction *transaction = [self new];
+    transaction.reference = dict[@"reference"];
+    transaction.trans = dict[@"trans"];
+    transaction.redirecturl = dict[@"redirecturl"];
+    transaction.message = dict[@"message"];
+    
+    transaction.allResponseFields = dict;
+    return transaction;
+}
+
+@end

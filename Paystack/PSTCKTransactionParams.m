817bf34eda8634423c3b30b5971283a798a70172
ibrahimlawal <ibrahim@lawal.me>
2016-10-19 23:17:04 +0100

    Add Transaction Params

---
+++ Paystack/PSTCKTransactionParams.m
@@ -0,0 +1,44 @@
+//
+//  PSTCKCardParams.m
+//  Paystack
+//
+
+#import "PSTCKTransactionParams.h"
+#import "PSTCKCardValidator.h"
+#import "PaystackError.h"
+#import "PSTCKRSA.h"
+
+@implementation PSTCKTransactionParams
+
+@synthesize additionalAPIParameters = _additionalAPIParameters;
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _additionalAPIParameters = @{};
+    }
+    return self;
+}
+
+
+#pragma mark - 
+
+#pragma mark - PSTCKFormEncodable
+
++ (NSString *)rootObjectName {
+    return @"";
+}
+
++ (NSDictionary *)propertyNamesToFormFieldNamesMapping {
+    return @{
+             @"email": @"email",
+             @"amount": @"amount",
+             @"reference": @"reference",
+             @"subaccount": @"subaccount",
+             @"transaction_charge": @"transactionCharge",
+             @"bearer": @"bearer",
+             @"metadata": @"metadata",
+             };
+}
+
+@end
---
+++ Paystack/PublicHeaders/PSTCKTransactionParams.h
@@ -0,0 +1,22 @@
+//
+//  PSTCKTransactionParams.h
+//  Paystack
+//
+
+#import <Foundation/Foundation.h>
+#import "PSTCKFormEncodable.h"
+/**
+ *  Representation of a user's credit card details. You can assemble these with information that your user enters and
+ *  then create Paystack tokens with them using an PSTCKAPIClient. @see https://paystack.com/docs/api#cards
+ */
+@interface PSTCKTransactionParams : NSObject<PSTCKFormEncodable>
+
+@property (nonatomic, copy, nonnull) NSString *email;
+@property (nonatomic, nonnull) NSUInteger *amount;
+@property (nonatomic, copy, nullable) NSString *reference;
+@property (nonatomic, copy, nullable) NSString *subaccount;
+@property (nonatomic, nullable) NSUInteger *transactionCharge;
+@property (nonatomic, copy, nullable) NSString *bearer;
+@property (nonatomic, copy, nullable) NSString *metadata;
+
+@end

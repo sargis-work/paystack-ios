5771c7b1054c205bf24f94cb269837e3618a9449
Ibrahim Lawal <ibrahim@paystack.co>
2019-08-07 08:11:23 +0100

    Bump to version 3.0.12, build 16

--- Paystack/PublicHeaders/PSTCKAPIClient.h
+++ Paystack/PublicHeaders/PSTCKAPIClient.h
@@ -8,8 +8,8 @@
 #import <UIKit/UIViewController.h>
 #endif
 
-static NSString *const __nonnull PSTCKSDKVersion = @"3.0.6";
-static NSString *const __nonnull PSTCKSDKBuild = @"15";
+static NSString *const __nonnull PSTCKSDKVersion = @"3.0.12";
+static NSString *const __nonnull PSTCKSDKBuild = @"16";
 
 @class PSTCKCard, PSTCKCardParams, PSTCKTransactionParams, PSTCKToken;
 
